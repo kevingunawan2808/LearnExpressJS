1959329f805f38b26cfc2cfcd27bc67efaf76ab6
kevin <kevin.g.2808@gmail.com>
2017-09-16 17:19:55 +0700

    add table

--- app/Routes/TransactionRoutes.js
+++ app/Routes/TransactionRoutes.js
@@ -4,18 +4,28 @@ const express = require('express'),
 
 router.post('/rent',async(req,res)=>{
 	let assetNumber=req.body.assetNumber,
-		username=req.decoded.username
+		username=req.decoded.username,
+		hour=req.body.hour
 	if(assetNumber){
-		res.json(await Transaction.rent(assetNumber,username))
+		res.json(await Transaction.rent(assetNumber,username,hour))
 	}else{
 		res.send("Internal Server Error")
 	}
 })
 
-router.post('/getAsset',async(req,res)=>{
+router.post('/callAsset',async(req,res)=>{
 	let assetNumber=req.body.assetNumber
 	if(assetNumber){
-		res.json(await Transaction.getAsset(assetNumber))
+		res.json(await Transaction.callAsset(assetNumber))
+	}else{
+		res.send("Internal Server Error")
+	}
+})
+
+router.post('/getTransactions',async(req,res)=>{
+	let username=req.decoded.username
+	if(username){
+		res.json(await Transaction.getTransactions(username))
 	}else{
 		res.send("Internal Server Error")
 	}
--- app/models/Transaction.js
+++ app/models/Transaction.js
@@ -1,25 +1,48 @@
 const db = require('../DB/db')
 const moment = require('moment')
 
-exports.rent = async(assetNumber,username) =>{
+exports.rent = async(assetNumber,username,hour) =>{
 	try{
-		let values=[username,assetNumber,moment().format()]
-	  	let ins= await db.execute('INSERT INTO TRANSACTIONS (USERNAME,asset_Number,created_at) VALUES(?, ?, ?)', values)
-	  	return {username:username,assetNumber:assetNumber,status:"succes"}
+
+		let asset= await getAsset(assetNumber)
+		let values=[username,assetNumber,asset.price*hour,hour,moment().format()]
+	  	let ins= await db.execute('INSERT INTO TRANSACTIONS (USERNAME,asset_Number,payment,hour,created_at) VALUES(?, ?, ?, ?, ?)', values)
+	  	return {username:username,assetNumber:assetNumber,hour:hour,payment:asset.price*hour,status:"succes"}
 	}catch(err){
 		console.log(err)
 		return err
 	}
 }
 
-exports.getAsset=async(assetNumber) =>{
+exports.getTransactions = async(username) =>{
+	try{
+		let values=[username]
+	  	let result= (await db.execute('SELECT ASSET_NUMBER,created_at,payment,hour FROM TRANSACTIONS WHERE USERNAME= ?', values))[0]
+	  	return {result:result,status:"succes"}
+	}catch(err){
+		console.log(err)
+		return err
+	}
+}
+
+exports.callAsset=async(assetNumber) =>{
+	try{
+	  	let result= await getAsset(assetNumber)
+	  	return result
+	}catch(err){
+		console.log(err)
+		return err
+	}
+}
+
+const getAsset=async(assetNumber)=>{
 	try{
 		let values=[assetNumber]
-	  	let result= (await db.execute('SELECT ASSET_NUMBER,TYPE FROM ASSETS WHERE ASSET_NUMBER= ?', values))[0]
+	  	let result= (await db.execute('SELECT ASSET_NUMBER,TYPE,PRICE FROM ASSETS WHERE ASSET_NUMBER= ?', values))[0]
 	  	if(!result.length){
 	  		return {status:"error",message:"Invalid Asset"}
 	  	}
-	  	return {assetNumber:result[0].ASSET_NUMBER,type:result[0].TYPE,status:"succes"}
+	  	return {assetNumber:result[0].ASSET_NUMBER,type:result[0].TYPE,price:result[0].PRICE,status:"succes"}
 	}catch(err){
 		console.log(err)
 		return err
